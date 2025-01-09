e93f2e23e97e868679592d67d5fc682529a1ca4d
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2025-01-09 14:17:30 +0530

    Added command line parsing
    go run main.go server --port 3001
    go run main.go client
    
    This is working fine.

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Glad hello!!")
+	// Defining the commands
+	serverCmd := flag.NewFlagSet("server", flag.ExitOnError)
+	serverPort := serverCmd.Int("port", 3000, "Server port")
+
+	clientCmd := flag.NewFlagSet("client", flag.ExitOnError)
+	remoteSystem := clientCmd.String("server", "127.0.0.1:3000", "Remote address")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Can be run in either client or server mode")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "server":
+		serverCmd.Parse(os.Args[2:])
+		fmt.Println("Running in server mode on", *serverPort)
+
+	case "client":
+		clientCmd.Parse(os.Args[2:])
+		fmt.Println("Running on client mode", *remoteSystem)
+
+	default:
+		fmt.Println("Show usage")
+	}
 }
