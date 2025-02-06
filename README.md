639e03f19056df5cf78a7b4963982ed0a231d4a5
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2025-02-06 16:04:48 +0530

    Code for simple socket server added
    The LDAP command connection is on server is coming correctly

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ This a go module which would have multiple client and multiple server ( which ac
 Able to run the application using command
 
 `go run main.go`
+
+To run the application is server mode
+`./glad server --port 3001`
+
+In client mode
+`./glad client --port 3001`
+
+LDAP Client code can be made using the following command
+
+`ldapsearch -D "cn=Directory Manager" -w "pass" -p 389 -h server.example.com -b "dc=example,dc=com" -s sub -x "(objectclass=*)"`
--- server/server.go
+++ server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"fmt"
+	"log"
+	"net"
 
 	"debojyoti.majumder/glad/commands"
 )
@@ -14,7 +16,44 @@ func initCommands() {
 	cmdMgr.AddCommands(&commands.RemoveNodeToClusterCommand{})
 }
 
+func proccessLDAPClient(client net.Conn) {
+	fmt.Println("Processing client connection")
+	client.Close()
+}
+
+func startLdapService(port int) {
+	connectionString := fmt.Sprintf(":%d", port)
+	fmt.Println("Starting server on port", port)
+
+	// Opening the port connection
+	connection, err := net.Listen("tcp", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Deffering the connection closure
+	defer connection.Close()
+
+	for {
+		client, err := connection.Accept()
+		if err != nil {
+			fmt.Println("Not able to process client")
+			continue
+		}
+
+		// Once the connection is accepted packet parsing is done in
+		// aysnc way
+		go proccessLDAPClient(client)
+	}
+}
+
+func startMangementService(port int) {
+	fmt.Printf("Started management service on port %d\n", port)
+}
+
 func StartServer(port int) {
 	initCommands()
-	fmt.Println("Starting server on port", port)
+
+	go startMangementService(port + 1)
+	startLdapService(port)
 }
